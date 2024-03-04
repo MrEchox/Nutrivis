856103d47a4f34a50927f3f9817cad69e86a4a91
Alanas Švažas <asalanys@gmail.com>
2024-03-04 18:32:25 +0200

    Testing
    Added welcome text to index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Welcome!</Text>
       <Text style={styles.title}>Index/home tab</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
