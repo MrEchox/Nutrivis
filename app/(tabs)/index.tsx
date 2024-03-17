bc94389855f527c89e8a9c3ea7a53e5034c49545
Perriup <zymogyvis@gmail.com>
2024-03-17 22:33:33 +0200

    -moved calculations and database check button to settings -added database integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,184 +4,21 @@ import { Text, View } from '@/components/Themed';
 import { calculateStepGoal } from '@/src/util/goal_calculations';
 import { calculateRecommendedCalories } from '@/src/util/goal_calculations';
 import {Picker} from '@react-native-picker/picker'
+import {Barcode_Food} from '@/src/object_classes/barcode_food'
 
-//---Step Tab---
-// const TabOneScreen = () => {
-//   const [age, setAge] = useState('');
-//   const [gender, setGender] = useState('male');
-//   const [height, setHeight] = useState('');
-//   const [weight, setWeight] = useState('');
-//   const [activityLevel, setActivityLevel] = useState('sedentary');
-//   const [stepGoal, setStepGoal] = useState(0);
-
-
-//   // Function to calculate step goal
-//   const handleCalculateStepGoal = () => {
-//     const calculatedStepGoal = calculateStepGoal(age, gender, height, weight, activityLevel);
-//     setStepGoal(calculatedStepGoal);
-//   };
-
-//---Calorie Tab---
 const TabOneScreen = () => {
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('male');
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [activityLevel, setActivityLevel] = useState('sedentary');
-  const [calorieGoal, setCalorieGoal] = useState(0);
-  const [weightObjective, setWeightObjective] = useState(0);
 
-  // Function to calculate recomended calories
-  const handleCalculateRecommendedCalories = () => {
-    const calculatedRecommendedCalories = calculateRecommendedCalories(age, gender, height, weight, activityLevel, weightObjective);
-    setCalorieGoal(calculatedRecommendedCalories);
-  };
-
-  return (
-    //---Steps---
-    // <View style={styles.container}>
-    //   <Text style={styles.title}>Index/home tab</Text>
-    //   <View style={styles.inputContainer}>
-    //     <Text style={styles.label}>Amžius:</Text>
-    //     <TextInput
-    //       style={styles.input}
-    //       placeholder="Įveskite savo amžių"
-    //       keyboardType="numeric"
-    //       onChangeText={(text) => setAge(text)}
-    //     />
-    //   </View>
-    //   <View style={styles.inputContainer}>
-    //     <Text style={styles.label}>Lytis</Text>
-    //     <Picker
-    //       selectedValue={gender}
-    //       placeholder="Pasirinkite lytį"
-    //       onValueChange={(itemValue) => setGender(itemValue)}
-    //       style={styles.input}
-    //     >
-    //       <Picker.Item label="Vyras" value="male" />
-    //       <Picker.Item label="Moteris" value="female" />
-    //     </Picker>
-    //   </View>
-    //   <View style={styles.inputContainer}>
-    //     <Text style={styles.label}>Ūgis</Text>
-    //     <TextInput
-    //       style={styles.input}
-    //       placeholder="Įveskite savo ūgį (cm)"
-    //       keyboardType="numeric"
-    //       onChangeText={(text) => setHeight(text)}
-    //     />
-    //   </View>
-    //   <View style={styles.inputContainer}>
-    //     <Text style={styles.label}>Svoris</Text>
-    //     <TextInput
-    //       style={styles.input}
-    //       placeholder="Įveskite savo svorį (kg)"
-    //       keyboardType="numeric"
-    //       onChangeText={(text) => setWeight(text)}
-    //     />
-    //   </View>
-    //   <View style={styles.inputContainer}>
-    //     <Text style={styles.label}>Fizinio aktyvumo lygis</Text>
-    //     <Picker
-    //       selectedValue={activityLevel}
-    //       onValueChange={(itemValue) => setActivityLevel(itemValue)}
-    //       style={styles.input}
-    //     >
-    //       <Picker.Item label="Neaktyvus" value="sedentary" />
-    //       <Picker.Item label="Mažas" value="lightly active" />
-    //       <Picker.Item label="Vidutinis" value="moderately active" />
-    //       <Picker.Item label="Didelis" value="very active" />
-    //       <Picker.Item label="Labai didelis" value="extra active" />
-    //     </Picker>
-    //   </View>
-    //   <Button title="Apskaičiuoti" onPress={handleCalculateStepGoal} />
-    //   <Text>Rekomenduojamas žingsnių skaičius per dieną: {stepGoal}</Text>
-    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    // </View>
+return (
 
     //---Calories---
     <View style={styles.container}>
       <Text style={styles.title}>Welcome!!!</Text>
       <Text style={styles.title}>Index/home tab</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Amžius:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Įveskite savo amžių"
-          keyboardType="numeric"
-          onChangeText={(text) => setAge(text)}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Lytis</Text>
-        <Picker
-          selectedValue={gender}
-          placeholder="Pasirinkite lytį"
-          onValueChange={(itemValue) => setGender(itemValue)}
-          style={styles.input}
-        >
-          <Picker.Item label="Vyras" value="male" />
-          <Picker.Item label="Moteris" value="female" />
-        </Picker>
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Ūgis</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Įveskite savo ūgį (cm)"
-          keyboardType="numeric"
-          onChangeText={(text) => setHeight(text)}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Svoris</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Įveskite savo svorį (kg)"
-          keyboardType="numeric"
-          onChangeText={(text) => setWeight(text)}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Fizinio aktyvumo lygis</Text>
-        <Picker
-          selectedValue={activityLevel}
-          onValueChange={(itemValue) => setActivityLevel(itemValue)}
-          style={styles.input}
-        >
-          <Picker.Item label="Neaktyvus" value="sedentary" />
-          <Picker.Item label="Mažas" value="lightly active" />
-          <Picker.Item label="Vidutinis" value="moderately active" />
-          <Picker.Item label="Didelis" value="very active" />
-          <Picker.Item label="Labai didelis" value="extra active" />
-        </Picker>
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Jūsų tikslas</Text>
-        <Picker
-          selectedValue={weightObjective}
-          onValueChange={(itemValue) => setWeightObjective(itemValue)}
-          style={styles.input}
-        >
-          <Picker.Item label="Ekstremalus svorio metimas" value="extreme loss" />
-          <Picker.Item label="Svorio metimas" value="loss" />
-          <Picker.Item label="Svorio palaikymas" value="maintain" />
-          <Picker.Item label="Svorio priaugimas" value="gain" />
-          <Picker.Item label="Ekstremalus svorio priaugimas" value="extreme gain" />
-        </Picker>
-      </View>
-      <Button title="Apskaičiuoti" onPress={handleCalculateRecommendedCalories} />
-      <Text>Rekomenduojamas kalorijų kiekis per dieną svoriui palaikyti: {calorieGoal}</Text>
-      {/*1g of Carbs = 4cal 50% of all calories*/}
-      <Text>Rekomenduojamas angliavandenių kiekis gramais: {Math.round(calorieGoal * 0.5 / 4)}</Text>
-      {/*1g of Fat = 9cal 20% of all calories*/}
-      <Text>Rekomenduojamas riebalų kiekis gramais: {Math.round(calorieGoal * 0.2 / 9)}</Text>
-      {/*1g of Protein = 4cal 30% of all calories*/}
-      <Text>Rekomenduojamas baltymų kiekis gramais: {Math.round(calorieGoal * 0.3 / 4)}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/object_classes/barcode_food.js
@@ -0,0 +1,62 @@
+import {collection, addDoc} from "firebase/firestore";
+import {db} from "../../firebaseConfig.js";
+
+
+// This is the class for the Barcode_Food object. This object is used to store the nutritional information of a food item.
+// Since this is not TypeScript, we have to manually check the types of the parameters. 
+// If the types are not correct, we throw an error.
+// This class also contains functions to save the object to the database.
+// Additional functions can be added as needed.
+export class Barcode_Food {
+        constructor(name, barcode, calories, carbs, sugars, fat, protein, sodium, measuring_unit) {
+        if (typeof name !== 'string') {
+            throw new Error('Name must be a string');
+        }
+        if (typeof barcode !== 'number') {
+            throw new Error('Barcode must be a number');
+        }
+        if (typeof calories !== 'number') {
+            throw new Error('Calories must be a number');
+        }
+        if (typeof carbs !== 'number') {
+            throw new Error('Carbs must be a number');
+        }
+        if (typeof sugars !== 'number') {
+            throw new Error('Sugars must be a number');
+        }
+        if (typeof fat !== 'number') {
+            throw new Error('Fat must be a number');
+        }
+        if (typeof protein !== 'number') {
+            throw new Error('Protein must be a number');
+        }
+        if (typeof sodium !== 'number') {
+            throw new Error('Sodium must be a number');
+        }
+        if (typeof measuring_unit !== 'string') {
+            throw new Error('Measuring unit must be a string');
+        }
+
+        this.barcode = barcode;
+        this.name = name;
+        this.carbs = carbs;
+        this.sugars = sugars;
+        this.protein = protein;
+        this.fat = fat;
+        this.calories = calories;
+    }
+
+    // This function saves the object to the database.
+    async save() {
+        const docRef = await addDoc(collection(db, "barcode_food_not_verified"), {
+            barcode: this.barcode,
+            name: this.name,
+            carbs: this.carbs,
+            sugars: this.sugars,
+            protein: this.protein,
+            fat: this.fat,
+            calories: this.calories
+        });
+        console.log("Document written with ID: ", docRef.id);
+    }
+}
