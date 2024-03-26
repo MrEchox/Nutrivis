bed888164e36243652355f9ee44fb758e1bc8d1b
Alanas Švažas <asalanys@gmail.com>
2024-03-27 01:38:43 +0200

    PK365-36 Added new page for topics
    Added new page with selectable sub-pages on dieting topics.
    
    PK365-20 - Changed the view to scrollview

---
+++ app/(tabs)/dieting.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View, FlatList, ScrollView } from 'react-native';
+import { Link } from 'expo-router';
+
+
+const pages = [
+    { title: 'Kalorijos', link: '../dieting/topics/calories' },
+    { title: 'Angliavandeniai', link: '../dieting/topics/carbs' },
+    { title: 'Baltymai', link: '../dieting/topics/protein' },
+    { title: 'Riebalai', link: '../dieting/topics/fats' },
+    { title: 'Skaidulos', link: '../dieting/topics/fiber' },
+    { title: 'Hidratacija', link: '../dieting/topics/hydration'},
+    { title: 'Papildai', link: '../dieting/topics/supplements'},
+  // Add more pages as needed
+];
+
+const PageList = () => {
+    const renderItem = ({ item }) => (
+        <Link href={item.link} style={{ padding: 10, fontSize: 20, borderBottomWidth: 1 }}>
+            ● {item.title}
+        </Link>
+    );
+
+    return (
+        <View>
+            <FlatList
+                data={pages}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.title}
+            />
+        </View>
+    );
+};
+
+export default PageList;
