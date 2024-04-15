45e30c9679509c87120b7e918abc105269114bbd
Alanas Švažas <asalanys@gmail.com>
2024-04-15 23:03:23 +0300

    Few changes
    Nepaejo padaryt naujo index page nes problemos su stack'ais

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -8,10 +8,10 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
+        <Text style={styles.title}>Šis puslapis neegzistuoja.</Text>
 
         <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
+          <Text style={styles.linkText}>Grįžti atgal!</Text>
         </Link>
       </View>
     </>
