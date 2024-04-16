9c904c129087940691a99889bfad9c26ec59a2a1
Perriup <zymogyvis@gmail.com>
2024-04-16 23:51:50 +0300

    -trying to add log in

--- src/object_classes/food_object_barcode.js
+++ src/object_classes/food_object_barcode.js
@@ -1,5 +1,5 @@
 import {collection, addDoc} from "firebase/firestore";
-import {db} from "../../firebase.config.js/index.js";
+import {db} from "../../firebase.config.js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
