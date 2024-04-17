91da23e969163ab4a0fc515376619ff00928c799
Ifz91Mamertas <79456572+Ifz91Mamertas@users.noreply.github.com>
2024-04-17 23:21:54 +0300

    PK365-51-Fill_out_more_information_on_dieting

--- app/dieting/topics/fats.tsx
+++ app/dieting/topics/fats.tsx
@@ -7,51 +7,49 @@ export default function Page() {
     return (
         <View style={styles.container}>
           <ScrollView>
-            <Text style={styles.title}>Lorem?</Text>
+          <Text style={styles.title}>Kas yra lipidai?</Text>
 
-            <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum nulla a nibh feugiat facilisis. Suspendisse a metus a mi dapibus tristique. 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales mauris id condimentum molestie. Phasellus quis eleifend arcu, eu eleifend justo. 
-            Sed sapien velit, laoreet vel porta at, finibus ac mi. Integer laoreet ipsum a lacus facilisis fermentum. 
-            Fusce et erat commodo, aliquam risus eget, fermentum enim. Nulla iaculis orci vel dignissim consectetur. 
-            Donec auctor tempor ligula in ultricies. Praesent vestibulum condimentum porta. Proin consequat eros lorem.
-            </Text>
+<Text style={styles.text}>
+Lipidai – svarbus organizmo energijos bei endogeninio vandens šaltinis (po galutinės 100 g lipidų oksidacijos susidaro 107,1 g vandens).
+</Text>
 
-            <Text style={styles.text}>
-            Ut nec orci placerat, congue orci quis, interdum dui. In a vestibulum arcu. Aenean erat dui, placerat sed ipsum quis, condimentum congue eros. 
-            Cras porttitor venenatis neque id consequat. Duis a pulvinar diam. Sed nec arcu ipsum. Pellentesque at laoreet ipsum. Nam rhoncus quam ut est maximus, 
-            ut efficitur mauris lobortis. Curabitur eget euismod velit, a viverra lorem. Donec viverra mi id quam pellentesque pretium. 
-            Etiam leo massa, auctor vel ultricies vel, porta id nibh. Ut eu elementum ipsum. Etiam sagittis turpis eu tristique mollis.
-            </Text>
+<Text style={styles.text}>
+Riebalai gali būti augalinės arba gyvulinės kilmės.
+</Text>
 
-            <Text style={styles.title}>Ipsum?</Text>
+<Text style={styles.text}>
+Augalinės kilmės lipiduose yra būtinų organizmui nesočiųjų riebiųjų rūgščių (linolino, linoleno, arachido), kurios atlieka vitaminų vaidmenį.
+</Text>
 
-            <Text style={styles.text}>
-            Ut varius nisl et tempor convallis. Nunc lobortis quam vel tortor tincidunt molestie. Duis eu eleifend massa. 
-            Cras libero nunc, euismod sit amet diam et, dapibus auctor lectus. Donec congue sapien nisi, tempor auctor purus consectetur vel. 
-            Morbi sit amet gravida velit, quis maximus nisi. Quisque eu porta leo. In ut odio tempus, ultrices nulla et, tempor nunc. 
-            Fusce egestas ultrices fringilla. Vestibulum mattis, nulla sed porta tincidunt, sem purus faucibus diam, nec fermentum neque sapien at eros. 
-            Proin eget purus non leo tempor finibus.
-            </Text>
+<Text style={styles.text}>
+Lipidai apsaugo nuo oksidacijos ir suirimo riebaluose tirpstančius vitaminus.
+</Text>
 
-            <Text style={styles.text}>
-            Cras eros dolor, finibus non sodales ut, accumsan non orci. Etiam venenatis molestie tristique. 
-            Suspendisse dapibus nulla id bibendum vestibulum. Donec sed vestibulum sapien. Duis ullamcorper vestibulum massa ut tempor. 
-            Nam libero nisi, tincidunt eget lorem euismod, ornare iaculis eros. Sed blandit massa ac tellus auctor mollis. 
-            In pretium, leo quis accumsan euismod, tortor mi pharetra erat, ac rutrum massa purus non est. Quisque pulvinar lobortis risus, ac molestie elit tempor elementum.
-            </Text>
+<Text style={styles.text}>
+Organizmui labai svarbios nesočiosios riebiosios rūgštys, biologiškai jos yra vertingesnės nei sočiosios riebiosios rūgštys. 
+</Text>
 
-            <Text style={styles.title}>Dolor sit amet?</Text>
+<Text style={styles.text}>
+Todėl augaliniai aliejai (ypač nerafinuoti) yra žymiai naudingesni nei kieti gyvuliniai riebalai. 
+</Text>
 
-            <Text style={styles.text}>
-            Ut eleifend odio felis, eu efficitur turpis vehicula sed. Quisque vulputate, lorem et ultrices hendrerit, magna ante venenatis orci, 
-            vel mattis nisi sapien nec enim. Etiam justo purus, dignissim eu leo sed, congue dictum neque. Nam aliquet turpis vel sapien vulputate fringilla. 
-            Sed ut augue arcu. Morbi in elit at urna porta tristique. Maecenas quis fermentum diam. Aliquam eu tincidunt ipsum, sit amet vehicula lacus. 
-            Nunc pellentesque vehicula porttitor. Donec bibendum mi turpis, facilisis molestie nisl imperdiet eu. Pellentesque non auctor lacus. 
-            Etiam pretium facilisis bibendum. Praesent vel massa a ante ornare iaculis at vel ipsum.
-            </Text>
+<Text style={styles.text}>
+Nesočiosios riebiosios rūgštys padeda pašalinti iš organizmo cholesterino perteklių per viškinamąjį traktą. 
+</Text>
 
-            <Text style={styles.refference}>Šaltinis:{'\n'}lipsum.com</Text>
+<Text style={styles.text}>
+Nesočiosios riebiosios rūgštys žmogaus organizme nėra sintezuojamos, todėl jos yra nepakeičiamos, o produktai, kuriuose jų yra būtinai turi būti maisto racione.
+</Text>
+
+<Text style={styles.text}>
+Kaip maisto papildai dažniausiai naudojami dvejopi polinesočiųjų riebiųjų rūgščių kompleksai – omega-3 ir omega-6. 
+</Text>
+
+<Text style={styles.text}>
+Jos kaupiasi galvos smegenų ir reprodukcinių ląstelių membranose.
+</Text>
+
+<Text style={styles.refference}>Šaltinis:{'\n'}https://maistassportui.lt/straipsniai/ivairus/lipidai/</Text>
           </ScrollView>
         </View>
     )
