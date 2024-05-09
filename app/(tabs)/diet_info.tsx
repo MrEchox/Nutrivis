4dd755c3197e1d9a5b41295a81cc808205776baf
arasmek <125920478+arasmek@users.noreply.github.com>
2024-05-09 20:37:45 +0300

    Diet Style #1

--- app/(tabs)/diet_info.tsx
+++ app/(tabs)/diet_info.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'expo-router';
-import { View, FlatList, Text, useColorScheme, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, useColorScheme, StyleSheet, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { commonStyles } from '../../components/commonStyles';
@@ -8,116 +8,132 @@ import { commonStyles } from '../../components/commonStyles';
 const prefix = '@Topic:';
 
 const pages = [
-    { title: '● Kalorijos',               quizId: 'calories_quiz' },
-    { title: '● Angliavandeniai',         quizId: 'carbs_quiz' },
-    { title: '● Baltymai',                quizId: 'protein_quiz' },
-    { title: '● Riebalai - lipidai',      quizId: 'fats_quiz' },
-    { title: '● Skaidulos',               quizId: 'fiber_quiz' },
-    { title: '● Hidratacija',             quizId: 'hydration_quiz' },
-    { title: '● Papildai',                quizId: 'supplements_quiz' },
-    { title: '● Sportavimo tipai',        quizId: 'exercise_types_quiz' },
-    { title: '● Maisto valgymo laikas',   quizId: 'eating_time_quiz' },
-    { title: '● Maisto planavimas',       quizId: 'meal_planning_quiz' },
-    { title: '● Nutukimas',               quizId: 'obesity_quiz' },
-    { title: '● Anoreksija',              quizId: 'anorexia_quiz' },
-    { title: '● Emocinis valgymas',       quizId: 'emotional_eating_quiz' },
+  { title: '● Kalorijos', quizId: 'calories_quiz' },
+  { title: '● Angliavandeniai', quizId: 'carbs_quiz' },
+  { title: '● Baltymai', quizId: 'protein_quiz' },
+  { title: '● Riebalai - lipidai', quizId: 'fats_quiz' },
+  { title: '● Skaidulos', quizId: 'fiber_quiz' },
+  { title: '● Hidratacija', quizId: 'hydration_quiz' },
+  { title: '● Papildai', quizId: 'supplements_quiz' },
+  { title: '● Sportavimo tipai', quizId: 'exercise_types_quiz' },
+  { title: '● Maisto valgymo laikas', quizId: 'eating_time_quiz' },
+  { title: '● Maisto planavimas', quizId: 'meal_planning_quiz' },
+  { title: '● Nutukimas', quizId: 'obesity_quiz' },
+  { title: '● Anoreksija', quizId: 'anorexia_quiz' },
+  { title: '● Emocinis valgymas', quizId: 'emotional_eating_quiz' },
   // Add more pages as needed
 ];
 
 const PageList = () => {
 
-    const [completedQuizzes, setCompletedQuizzes] = useState([]);
+  const [completedQuizzes, setCompletedQuizzes] = useState([]);
 
-    const colorScheme = useColorScheme();
-    const router = useRouter();
+  const colorScheme = useColorScheme();
+  const router = useRouter();
 
-    const themeBackground = colorScheme === 'light' ? commonStyles.lightBackground : commonStyles.darkBackground;
-    const themeTextStyle = colorScheme === 'light' ? commonStyles.lightThemeText : commonStyles.darkThemeText;
-    const themeSvg = colorScheme === 'light' ? '#ffffff' : '#003049';
-    useEffect(() => {
-        const fetchCompletedQuizzes = async () => {
-            const keys = pages.map((item) => prefix + item.quizId);
-            const results = await AsyncStorage.multiGet(keys);
+  const themeBackground = colorScheme === 'light' ? commonStyles.lightBackground : commonStyles.darkBackground;
+  const themeTextStyle = colorScheme === 'light' ? commonStyles.lightThemeText : commonStyles.darkThemeText;
+  const themeContainer = colorScheme === 'light' ? commonStyles.lightContainer : commonStyles.darkContainer;
 
-            const completed = results
-                .filter(([key, value]) => value === 'true')
-                .map(([key, _]) => key.replace(prefix, ''));
+  useEffect(() => {
+    const fetchCompletedQuizzes = async () => {
+      const keys = pages.map((item) => prefix + item.quizId);
+      const results = await AsyncStorage.multiGet(keys);
 
-            setCompletedQuizzes(completed);
-        };
+      const completed = results
+        .filter(([key, value]) => value === 'true')
+        .map(([key, _]) => key.replace(prefix, ''));
 
-        fetchCompletedQuizzes();
-    }, []);
-
-    const renderItem = ({ item }) => {
-        const isCompleted = completedQuizzes.includes(item.quizId);
-
-        return (
-                <TouchableOpacity
-                    style={[
-                        themeTextStyle,
-                        { padding: 10 },
-                        isCompleted ? { backgroundColor: 'lightgreen' } : {},
-                    ]}
-                    onPress={() => router.push(`../dieting/quiz_screen?quizId=${item.quizId}`)}
-                >
-                    <Text style={themeTextStyle}>{item.title}</Text>
-                </TouchableOpacity>
-        );
+      setCompletedQuizzes(completed);
     };
-    
 
-    return (
-        <View style={[themeBackground, {flex: 1}]}>
-            <FlatList
-                data={pages}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.title}
-            />
+    fetchCompletedQuizzes();
+  }, []);
+
+  const handleItemClick = (quizId) => {
+    router.push(`../dieting/quiz_screen?quizId=${quizId}`);
+  };
+
+  const renderColumns = () => {
+    const numItems = pages.length;
+    const numColumns = 2;
+    const itemsPerColumn = Math.ceil(numItems / numColumns);
+    const columns = [];
+
+    for (let i = 0; i < numColumns; i++) {
+      const start = i * itemsPerColumn;
+      const end = Math.min(start + itemsPerColumn, numItems);
+      const columnItems = pages.slice(start, end);
+
+      columns.push(
+        <View key={i} style={styles.column}>
+          {columnItems.map((item) => (
+            <View style={[commonStyles.mainStatsContainer, themeContainer]}>
+              <TouchableOpacity
+                key={item.quizId}
+                style={[
+                  styles.item,
+                  completedQuizzes.includes(item.quizId) ? { backgroundColor: 'lightgreen' } : {},
+                ]}
+                onPress={() => handleItemClick(item.quizId)}
+              >
+                <Text style={themeTextStyle}>{item.title}</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
-    );
+      );
+    }
+
+    return columns;
+  };
+
+  return (
+    <View style={[themeBackground, styles.container]}>
+      <View style={[styles.columnContainer, commonStyles.mainStatsContainer, themeContainer, {marginLeft:20}]}>
+        <View style={[styles.column, { alignItems: 'center' }]}>
+          <Image
+            source={require('../../assets/images/nutrivis_magnif.png')}
+            style={[{ width: 121, height: 167, marginBottom: 20 }]}
+          />
+        </View>
+        <View style={[styles.column, { alignItems: 'center' }]}>
+          <Text style={[themeTextStyle, { fontSize: 70 }]}>1/13</Text>
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        {renderColumns()}
+      </View>
+    </View>
+  );
 };
 
 export default PageList;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      paddingTop: 30,
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    label: {
-      marginRight: 10,
-      paddingLeft: 10,
-      paddingBottom: 10,
-      fontWeight: "500",
-    },
-    inputContainer: {
-      width: '80%',
-      marginBottom: 20,
-      borderWidth: 1,
-      borderRadius: 5,
-      borderColor: '#ccc',
-      alignSelf: 'center',
-    },
-    input: {
-      padding: 10,
-      borderWidth: 1,
-      borderRadius: 10,
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-    buttonContainer: {
-      borderRadius: 10,
-      overflow: 'hidden',
-      backgroundColor: '',
-    },
-  });
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    marginTop: 0,
+  },
+
+  listContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  column: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  columnContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    marginBottom: 10,
+  },
+});
